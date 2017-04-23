8caadbe8463fff2b0a2343f1fce912000de86679
stavdv <stavbarazani@gmail.com>
2017-04-23 12:53:35 +0300

    Add resize function for change the ct scans to 240x240x155

---
+++ Iter01_MVP_prototype/dataHandler/resize.m
@@ -0,0 +1,12 @@
+function resizeCt = resize(filepath,newName)
+
+CtImageInRegularSize = load_untouch_nii_gzip (filepath);
+volumeNiiImage = double(CtImageInRegularSize.img);
+figure;imshow(volumeNiiImage(:,:,3));
+resizeCt = CTresize2((volumeNiiImage) , 240 , 240, 155);
+figure;imshow(resizeCt(:,:,3));
+nii = make_nii(resizeCt);
+save_nii(nii ,newName);
+
+end
+

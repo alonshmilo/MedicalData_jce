29beae6625c8372992e697c2b91048d127f0d795
stavdv <stavbarazani@gmail.com>
2017-05-03 16:33:55 +0300

    Change convert.py

--- Iter02_DMC_construction/Yolo/BBox-Label-Tool/convert.py
+++ Iter02_DMC_construction/Yolo/BBox-Label-Tool/convert.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-    Created on Wed Dec  9 14:55:43 2015
     This script is to convert the txt annotation files to appropriate format needed by YOLO
-    @author: Guanghan Ning
-    Email: gnxr9@mail.missouri.edu
-    """
+"""
 
 import os
 from os import walk, getcwd
 from PIL import Image
 
-classes = ["stopsign"]
+classes = ["001"]
 
 def convert(size, box):
     dw = 1./size[0]
@@ -29,10 +26,10 @@ def convert(size, box):
 """-------------------------------------------------------------------"""
 
 """ Configure Paths"""
-mypath = "labels/stopsign_original/"
-outpath = "labels/stopsign/"
+mypath = "labels/001/"
+outpath = "labels/001/bones"
 
-cls = "stopsign"
+cls = "001"
 if cls not in classes:
     exit(0)
 cls_id = classes.index(cls)
@@ -50,19 +47,19 @@ print(txt_name_list)
 """ Process """
 for txt_name in txt_name_list:
     # txt_file =  open("Labels/stop_sign/001.txt", "r")
-    
+
     """ Open input text files """
     txt_path = mypath + txt_name
     print("Input:" + txt_path)
     txt_file = open(txt_path, "r")
     lines = txt_file.read().split('\r\n')   #for ubuntu, use "\r\n" instead of "\n"
-    
+
     """ Open output text files """
     txt_outpath = outpath + txt_name
     print("Output:" + txt_outpath)
     txt_outfile = open(txt_outpath, "w")
-    
-    
+
+
     """ Convert the data to YOLO format """
     ct = 0
     for line in lines:

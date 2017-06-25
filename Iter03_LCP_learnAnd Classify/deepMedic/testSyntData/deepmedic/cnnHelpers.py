0cdc9d09ec714e1055f9514f9d4d94916c8e179c
stavdv <stavbarazani@gmail.com>
2017-06-25 22:22:19 +0300

    Add deepMedic files to testSyntData iter03

---
+++ Iter03_LCP_learnAnd Classify/deepMedic/testSyntData/deepmedic/cnnHelpers.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2016, Konstantinos Kamnitsas
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the BSD license. See the accompanying LICENSE file
+# or read the terms at https://opensource.org/licenses/BSD-3-Clause.
+
+import numpy as np
+from genericHelpers import *
+import os
+
+# The API for these classes should resemble the API of Pathway and Cnn3d classes. But only what is needed by the sampling process of the training procedure.
+class PathwayWrapperForSampling(object):
+    # For CnnWrapperForSampling class.
+    def __init__(self, pathwayInstance) :
+        self._pType = pathwayInstance.pType()
+        self._subsFactor = pathwayInstance.subsFactor()
+        self._shapeOfInputTrainValTest = pathwayInstance.getShapeOfInput()
+        self._shapeOfOutputTrainValTest = pathwayInstance.getShapeOfOutput()
+    def pType(self):
+        return self._pType
+    def subsFactor(self):
+        return self._subsFactor
+    def getShapeOfInput(self):
+        return self._shapeOfInputTrainValTest
+    def getShapeOfOutput(self):
+        return self._shapeOfOutputTrainValTest
+        
+class CnnWrapperForSampling(object):
+    # Only for the parallel process used during training. So that it won't re-load theano etc. There was a problem with cnmem when reloading theano.
+    def __init__(self, cnn3dInstance) :
+        # Cnn
+        self.recFieldCnn = cnn3dInstance.recFieldCnn
+        self.batchSizeTrainValTest = [ cnn3dInstance.batchSize, cnn3dInstance.batchSizeValidation, cnn3dInstance.batchSizeTesting ]
+        self.finalTargetLayer_outputShapeTrainValTest = [cnn3dInstance.finalTargetLayer.outputShapeTrain,
+                                                        cnn3dInstance.finalTargetLayer.outputShapeVal,
+                                                        cnn3dInstance.finalTargetLayer.outputShapeTest ]
+        # Pathways related
+        self._numPathwaysThatRequireInput = cnn3dInstance.getNumPathwaysThatRequireInput()
+        self.numSubsPaths = cnn3dInstance.numSubsPaths
+        
+        self.pathways = []
+        for pathway_i in xrange(len(cnn3dInstance.pathways)) :
+            self.pathways.append( PathwayWrapperForSampling(cnn3dInstance.pathways[pathway_i]) )
+        
+    def getNumPathwaysThatRequireInput(self) :
+        return self._numPathwaysThatRequireInput
+    
+    
+def dump_cnn_to_gzip_file_dotSave(cnnInstance, filenameWithPathToSaveTo, logger=None) :
+    filenameWithPathToSaveToDotSave = os.path.abspath(filenameWithPathToSaveTo + ".save")
+    cnnInstance.freeGpuTrainingData(); cnnInstance.freeGpuValidationData(); cnnInstance.freeGpuTestingData();
+    # Clear out the compiled functions, so that they are not saved with the instance:
+    compiledFunctionTrain = cnnInstance.cnnTrainModel; cnnInstance.cnnTrainModel = ""
+    compiledFunctionVal = cnnInstance.cnnValidateModel; cnnInstance.cnnValidateModel = ""
+    compiledFunctionTest = cnnInstance.cnnTestModel; cnnInstance.cnnTestModel = ""
+    compiledFunctionVisualise = cnnInstance.cnnVisualiseFmFunction; cnnInstance.cnnVisualiseFmFunction = ""
+    
+    if logger <> None :
+        logger.print3("Saving network to: "+str(filenameWithPathToSaveToDotSave))
+    else:
+        print("Saving network to: "+str(filenameWithPathToSaveToDotSave))
+        
+    dump_object_to_gzip_file(cnnInstance, filenameWithPathToSaveToDotSave)
+    
+    if logger <> None :
+        logger.print3("Model saved.")
+    else:
+        print("Model saved.")
+        
+    # Restore instance's values, which were cleared for the saving of the instance:
+    cnnInstance.cnnTrainModel = compiledFunctionTrain
+    cnnInstance.cnnValidateModel = compiledFunctionVal
+    cnnInstance.cnnTestModel = compiledFunctionTest
+    cnnInstance.cnnVisualiseFmFunction = compiledFunctionVisualise
+    
+    return filenameWithPathToSaveToDotSave
+
+
+def calculateSubsampledImagePartDimensionsFromImagePartSizePatchSizeAndSubsampleFactor(imagePartDimensions, patchDimensions, subsampleFactor) :
+    """
+    This function gives you how big your subsampled-image-part should be, so that it corresponds to the correct number of central-voxels in the normal-part. Currently, it's coupled with the patch-size of the normal-scale. I.e. the subsampled-patch HAS TO BE THE SAME SIZE as the normal-scale, and corresponds to subFactor*patchsize in context.
+    When the central voxels are not a multiple of the subFactor, you get ceil(), so +1 sub-patch. When the CNN repeats the pattern, it is giving dimension higher than the central-voxels of the normal-part, but then they are sliced-down to the correct number (in the cnn_make_model function, right after the repeat).        
+    This function works like this because of getImagePartFromSubsampledImageForTraining(), which gets a subsampled-image-part by going 1 normal-patch back from the top-left voxel of a normal-scale-part, and then 3 ahead. If I change it to start from the top-left-CENTRAL-voxel back and front, I will be able to decouple the normal-patch size and the subsampled-patch-size. 
+    """
+    #if patch is 17x17, a 17x17 subPart is cool for 3 voxels with a subsampleFactor. +2 to be ok for the 9x9 centrally classified voxels, so 19x19 sub-part.
+    subsampledImagePartDimensions = []
+    for rcz_i in xrange(len(imagePartDimensions)) :
+        centralVoxelsInThisDimension = imagePartDimensions[rcz_i] - patchDimensions[rcz_i] + 1
+        centralVoxelsInThisDimensionForSubsampledPart = int(ceil(centralVoxelsInThisDimension*1.0/subsampleFactor[rcz_i]))
+        sizeOfSubsampledImagePartInThisDimension = patchDimensions[rcz_i] + centralVoxelsInThisDimensionForSubsampledPart - 1
+        subsampledImagePartDimensions.append(sizeOfSubsampledImagePartInThisDimension)
+    return subsampledImagePartDimensions
+
+def calculateReceptiveFieldDimensionsFromKernelsDimListPerLayerForFullyConvCnnWithStrides1(kernDimPerLayerList) :
+    if not kernDimPerLayerList : #list is []
+        return 0
+    
+    numberOfDimensions = len(kernDimPerLayerList[0])
+    receptiveField = [1]*numberOfDimensions
+    for dimension_i in xrange(numberOfDimensions) :
+        for layer_i in xrange(len(kernDimPerLayerList)) :
+            receptiveField[dimension_i] += kernDimPerLayerList[layer_i][dimension_i] - 1
+    return receptiveField
+
+def checkReceptiveFieldFineInComparisonToSegmentSize(receptiveFieldDim, segmentDim) :
+    numberOfRFDim = len(receptiveFieldDim)
+    numberOfSegmDim = len(segmentDim)
+    if numberOfRFDim <> numberOfSegmDim :
+        print "ERROR: [in function checkReceptiveFieldFineInComparisonToSegmentSize()] : Receptive field and image segment have different number of dimensions! (should be 3 for both! Exiting!)"
+        exit(1)
+    for dim_i in xrange(numberOfRFDim) :
+        if receptiveFieldDim[dim_i] > segmentDim[dim_i] :
+            print "ERROR: [in function checkReceptiveFieldFineInComparisonToSegmentSize()] : The segment-size (input) should be at least as big as the receptive field of the model! This was not found to hold! Dimensions of Receptive Field:", receptiveFieldDim, ". Dimensions of Segment: ", segmentDim
+            return False
+    return True
+
+def checkKernDimPerLayerCorrect3dAndNumLayers(kernDimensionsPerLayer, numOfLayers) :
+    #kernDimensionsPerLayer : a list with sublists. One sublist per layer. Each sublist should have 3 integers, specifying the dimensions of the kernel at the corresponding layer of the pathway. eg: kernDimensionsPerLayer = [ [3,3,3], [3,3,3], [5,5,5] ] 
+    if kernDimensionsPerLayer == None or len(kernDimensionsPerLayer) <> numOfLayers :
+        return False
+    for kernDimInLayer in kernDimensionsPerLayer :
+        if len(kernDimInLayer) <> 3 :
+            return False
+    return True
+
+def checkSubsampleFactorEven(subFactor) :
+    for dim_i in xrange(len(subFactor)) :
+        if subFactor[dim_i]%2 <> 1 :
+            return False
+    return True
+
---
+++ Iter03_LCP_learnAnd Classify/deepMedic/testSyntData/deepmedic/frontEndModules/deepMedicNewModel.py
@@ -0,0 +1,174 @@
+# Copyright (c) 2016, Konstantinos Kamnitsas
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the BSD license. See the accompanying LICENSE file
+# or read the terms at https://opensource.org/licenses/BSD-3-Clause.
+
+import os
+
+from deepmedic import myLoggerModule
+
+from deepmedic.cnn3d import Cnn3d
+
+from deepmedic.frontEndModules.frontEndHelpers.parsingFilesHelpers import getAbsPathEvenIfRelativeIsGiven
+from deepmedic.frontEndModules.frontEndHelpers.parsingFilesHelpers import checkIfAllElementsOfAListAreFilesAndExitIfNot
+from deepmedic.frontEndModules.frontEndHelpers.parsingFilesHelpers import checkListContainsCorrectNumberOfCasesOtherwiseExitWithError
+from deepmedic.frontEndModules.frontEndHelpers.parsingFilesHelpers import checkThatAllEntriesOfAListFollowNameConventions
+
+from deepmedic.frontEndModules.frontEndHelpers.createModelParametersClass import CreateModelSessionParameters
+from deepmedic.frontEndModules.frontEndHelpers.preparationForSessionHelpers import makeFoldersNeededForCreateModelSession
+
+from deepmedic.cnnHelpers import dump_cnn_to_gzip_file_dotSave
+from deepmedic.genericHelpers import datetimeNowAsStr
+
+from deepmedic.genericHelpers import load_object_from_gzip_file
+
+class ModelConfig(object):
+    configStruct = {} #In here will be placed all read arguments.
+    def get(self,string1) :
+        return self.configStruct[string1] if string1 in self.configStruct else None
+    
+    #Optional but highly suggested.
+    MODEL_NAME = "modelName"
+    #[REQUIRED] Output:
+    FOLDER_FOR_OUTPUT = "folderForOutput" #MUST BE GIVEN
+    
+    #================ MODEL PARAMETERS =================
+    NUMB_CLASSES = "numberOfOutputClasses"
+    NUMB_INPUT_CHANNELS_NORMAL = "numberOfInputChannels"
+    
+    #===Normal pathway===
+    N_FMS_NORM = "numberFMsPerLayerNormal"
+    KERN_DIM_NORM = "kernelDimPerLayerNormal"
+    RESID_CONN_LAYERS_NORM = "layersWithResidualConnNormal"
+    LOWER_RANK_LAYERS_NORM = "lowerRankLayersNormal"
+    
+    #==Subsampled pathway==
+    USE_SUBSAMPLED = "useSubsampledPathway"
+    #The below should be mirroring the pathway, otherwise let them specify them but throw warning all around that receptive field should stay the same!
+    N_FMS_SUBS = "numberFMsPerLayerSubsampled"
+    KERN_DIM_SUBS = "kernelDimPerLayerSubsampled"
+    SUBS_FACTOR = "subsampleFactor"
+    RESID_CONN_LAYERS_SUBS = "layersWithResidualConnSubsampled"
+    LOWER_RANK_LAYERS_SUBS = "lowerRankLayersSubsampled"
+    
+    #==Extra hidden FC Layers. Final Classification layer is not included in here.
+    N_FMS_FC = "numberFMsPerLayerFC"
+    KERN_DIM_1ST_FC = "kernelDimFor1stFcLayer"
+    RESID_CONN_LAYERS_FC = "layersWithResidualConnFC"
+    
+    #Size of Image Segments
+    SEG_DIM_TRAIN = "segmentsDimTrain"
+    SEG_DIM_VAL = "segmentsDimVal"
+    SEG_DIM_INFERENCE = "segmentsDimInference"
+    
+    #==Batch Sizes===
+    #Required.
+    BATCH_SIZE_TR = "batchSizeTrain"
+    BATCH_SIZE_VAL = "batchSizeVal"
+    BATCH_SIZE_INFER = "batchSizeInfer"
+    
+    #Dropout Rates:
+    DROP_R_NORM = "dropoutRatesNormal"
+    DROP_R_SUBS = "dropoutRatesSubsampled"
+    DROP_R_FC = "dropoutRatesFc"
+    
+    #Initialization method of the kernel weights. Classic is what I was using for my first year. "Delving Deep" for journal.
+    INITIAL_METHOD = "initializeClassic0orDelving1"
+    #Activation Function for all convolutional layers:
+    ACTIV_FUNCTION = "relu0orPrelu1"
+    
+    #Batch Normalization
+    BN_ROLL_AV_BATCHES = "rollAverageForBNOverThatManyBatches"
+
+
+#The argument should be absolute path to the config file for the model to create.
+def deepMedicNewModelMain(modelConfigFilepath, absPathToPreTrainedModelGivenInCmdLine, listOfLayersToTransfer) :
+    print "Given Model-Configuration File: ", modelConfigFilepath
+    #Parse the config file in this naive fashion...
+    modelConfig = ModelConfig()
+    execfile(modelConfigFilepath, modelConfig.configStruct)
+    configGet = modelConfig.get #Main interface
+    
+    #Create Folders and Logger
+    mainOutputAbsFolder = getAbsPathEvenIfRelativeIsGiven(configGet(modelConfig.FOLDER_FOR_OUTPUT), modelConfigFilepath)
+    modelName = configGet(modelConfig.MODEL_NAME) if configGet(modelConfig.MODEL_NAME) else CreateModelSessionParameters.getDefaultModelName()
+    [folderForCnnModels,
+    folderForLogs] = makeFoldersNeededForCreateModelSession(mainOutputAbsFolder, modelName)
+    loggerFileName = folderForLogs + "/" + modelName + ".txt"
+    sessionLogger = myLoggerModule.MyLogger(loggerFileName)
+    
+    sessionLogger.print3("CONFIG: The configuration file for the model-creation session was loaded from: " + str(modelConfigFilepath))
+    
+    #Fill in the session's parameters.
+    createModelSessionParameters = CreateModelSessionParameters(
+                    cnnModelName=modelName,
+                    sessionLogger=sessionLogger,
+                    mainOutputAbsFolder=mainOutputAbsFolder,
+                    folderForSessionCnnModels=folderForCnnModels,
+                    #===MODEL PARAMETERS===
+                    numberClasses=configGet(modelConfig.NUMB_CLASSES),
+                    numberOfInputChannelsNormal=configGet(modelConfig.NUMB_INPUT_CHANNELS_NORMAL),
+                    #===Normal pathway===
+                    numFMsNormal=configGet(modelConfig.N_FMS_NORM),
+                    kernDimNormal=configGet(modelConfig.KERN_DIM_NORM),
+                    residConnAtLayersNormal=configGet(ModelConfig.RESID_CONN_LAYERS_NORM),
+                    lowerRankLayersNormal=configGet(ModelConfig.LOWER_RANK_LAYERS_NORM),
+                    #==Subsampled pathway==
+                    useSubsampledBool=configGet(modelConfig.USE_SUBSAMPLED),
+                    numFMsSubsampled=configGet(modelConfig.N_FMS_SUBS),
+                    kernDimSubsampled=configGet(modelConfig.KERN_DIM_SUBS),
+                    subsampleFactor=configGet(modelConfig.SUBS_FACTOR),
+                    residConnAtLayersSubsampled=configGet(ModelConfig.RESID_CONN_LAYERS_SUBS),
+                    lowerRankLayersSubsampled=configGet(ModelConfig.LOWER_RANK_LAYERS_SUBS),
+                    #==FC Layers====
+                    numFMsFc=configGet(modelConfig.N_FMS_FC),
+                    kernelDimensionsFirstFcLayer=configGet(modelConfig.KERN_DIM_1ST_FC),
+                    residConnAtLayersFc=configGet(ModelConfig.RESID_CONN_LAYERS_FC),
+                    #==Size of Image Segments ==
+                    segmDimTrain=configGet(modelConfig.SEG_DIM_TRAIN),
+                    segmDimVal=configGet(modelConfig.SEG_DIM_VAL),
+                    segmDimInfer=configGet(modelConfig.SEG_DIM_INFERENCE),
+                    #== Batch Sizes ==
+                    batchSizeTrain=configGet(modelConfig.BATCH_SIZE_TR),
+                    batchSizeVal=configGet(modelConfig.BATCH_SIZE_VAL),
+                    batchSizeInfer=configGet(modelConfig.BATCH_SIZE_INFER),
+                    #===Other Architectural Parameters ===
+                    activationFunction=configGet(modelConfig.ACTIV_FUNCTION),
+                    #==Dropout Rates==
+                    dropNormal=configGet(modelConfig.DROP_R_NORM),
+                    dropSubsampled=configGet(modelConfig.DROP_R_SUBS),
+                    dropFc=configGet(modelConfig.DROP_R_FC),
+                    #== Weight Initialization==
+                    initialMethod=configGet(modelConfig.INITIAL_METHOD),
+                    #== Batch Normalization ==
+                    bnRollingAverOverThatManyBatches=configGet(modelConfig.BN_ROLL_AV_BATCHES),
+                    )
+    
+    
+    createModelSessionParameters.sessionLogger.print3("\n===========    NEW CREATE-MODEL SESSION    ============")
+    createModelSessionParameters.printParametersOfThisSession()
+    
+    createModelSessionParameters.sessionLogger.print3("\n=========== Creating the CNN model ===============")
+    cnn3dInstance = Cnn3d()
+    cnn3dInstance.make_cnn_model(*createModelSessionParameters.getTupleForCnnCreation())
+    
+    if absPathToPreTrainedModelGivenInCmdLine <> None: # Transfer parameters from a previously trained model to the new one.
+        createModelSessionParameters.sessionLogger.print3("\n=========== Pre-training the new model ===============")
+        sessionLogger.print3("...Loading the pre-trained network. This can take a few minutes if the model is big...")
+        cnnPretrainedInstance = load_object_from_gzip_file(absPathToPreTrainedModelGivenInCmdLine)
+        sessionLogger.print3("The pre-trained model was loaded successfully from: " + str(absPathToPreTrainedModelGivenInCmdLine))
+        from deepmedic import cnnTransferParameters
+        cnn3dInstance = cnnTransferParameters.transferParametersBetweenModels(sessionLogger, cnn3dInstance, cnnPretrainedInstance, listOfLayersToTransfer)
+    
+    createModelSessionParameters.sessionLogger.print3("\n=========== Saving the model ===============")
+    if absPathToPreTrainedModelGivenInCmdLine <> None:
+        filenameAndPathToSaveModel = createModelSessionParameters.getPathAndFilenameToSaveModel() + ".initial.pretrained." + datetimeNowAsStr()
+    else:
+        filenameAndPathToSaveModel = createModelSessionParameters.getPathAndFilenameToSaveModel() + ".initial." + datetimeNowAsStr()
+    filenameAndPathWhereModelWasSaved =  dump_cnn_to_gzip_file_dotSave(cnn3dInstance, filenameAndPathToSaveModel, sessionLogger)
+    createModelSessionParameters.sessionLogger.print3("=========== Creation of the model: \"" + str(createModelSessionParameters.cnnModelName) +"\" finished =================")
+    
+    return (cnn3dInstance, filenameAndPathWhereModelWasSaved)
+
